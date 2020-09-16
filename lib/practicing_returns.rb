67ece12d470249141c74ef20f1540f81652bfaf8
David Mills <david_mills109@yahoo.com>
2020-09-16 00:51:41 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,13 +1,6 @@
 require 'pry'
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-end
+
 
 binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
